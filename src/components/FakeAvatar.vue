005633d6ab858b301e59c10c45b7d7af2864d41a
Jaycee Daily <alohajaycee@gmail.com>
2022-04-12 21:42:52 +0800

    Implement FakeAvatar

---
+++ src/components/FakeAvatar.vue
@@ -0,0 +1,21 @@
+<template>
+	<img class="inline object-cover rounded-full" :src="`https://avatars.dicebear.com/api/avataaars/${value}.svg?background=%23e2e2e2`" alt="Profile image"/>  
+</template>
+
+<script>
+export default {
+	name: "FakeAvatar",
+
+	props: {
+		value: {
+			required: false,
+			default: "test"
+		}
+	}
+
+}
+</script>
+
+<style>
+
+</style>
