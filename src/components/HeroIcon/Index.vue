2eb76533beaa560bb7c1292b8d2bbd01142ec6d8
Jaycee Daily <alohajaycee@gmail.com>
2022-06-03 23:51:15 +0800

    Create HeroIcon component

---
+++ src/components/HeroIcon/Index.vue
@@ -0,0 +1,42 @@
+<template>
+	<component :is="Icon"/>
+</template>
+<script>
+
+import * as SolidIcon from '@heroicons/vue/solid';
+import * as OutlineIcon from '@heroicons/vue/outline';
+import { computed } from '@vue/runtime-core';
+
+export default {
+	name: "HeroIcon",
+	props: {
+		type: {
+			type: String,
+			default: 'outline'
+		},
+		icon: {
+			type: String,
+			required: true
+		}
+	},
+	setup(props) {
+		
+		const Icon = computed(() => {
+
+			if(props.type === 'solid') {
+				return SolidIcon[props.icon];
+			}
+
+			return OutlineIcon[props.icon];
+		});
+
+		return {
+			Icon
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
